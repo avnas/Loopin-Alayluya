d7d9ee3d20e074156e07be9bb6c45ae47da3e8d8
RanjanSigdel <ranjansharma.8091@gmail.com>
2017-04-27 06:09:43 +0545

    button changed

--- app/src/main/java/alayluya/loopin/com/loopin_alayluya/Activities/MainActivity.java
+++ app/src/main/java/alayluya/loopin/com/loopin_alayluya/Activities/MainActivity.java
@@ -69,7 +69,8 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    @Override
+    /////////language option to keep here///////////////
+   /* @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -82,7 +83,7 @@ public class MainActivity extends AppCompatActivity
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
